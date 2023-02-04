33480ed711f721e88ca73080f95b05ce83d6ce31
jiji0704 <chocojiji0704@yahoo.co.jp>
2023-02-04 11:27:05 +0900

    修正1

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -51,7 +51,7 @@ class PrototypesController < ApplicationController
 
   def move_to_index
     unless user_signed_in?
-      redirect_to action: :index
+      redirect_to '/users/sign_in'
     end
   end
   
